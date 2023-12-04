022ffd0eaaa980af7ec53fe654c417ea9046dda7
bespechniyribak <bespechniyribak@gmail.com>
2023-12-05 00:02:39 +0100

    вернулся к началу

--- app.py
+++ app.py
@@ -1,40 +1,21 @@
-import os
-import sys
-import fcntl
-from aiogram import Bot, Dispatcher, types
+from aiogram import types
+from aiogram import Dispatcher
 from aiogram.types import Message
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.types import ReplyKeyboardRemove
 from aiogram import executor
 from logging import basicConfig, INFO
 from aiogram.types import BotCommand
-from data.config import ADMINS,DEFAULT_ADMINS
+from data.config import ADMINS, DEFAULT_ADMINS
 from handlers.user.catalog import process_catalog
 from loader import db, bot
 import handlers, requests
 from handlers import dp
 from handlers.user.menu import catalog, cart, delivery_status
 
-LOCK_PATH = "/tmp/bot.lock"
-DEFAULT_ADMINS = []  # Замените это на реальный список админов
-
-bot = Bot(token='6810545901:AAE3iPbhGYcaCV_auyHcjVYdlYClbIRK8oQ')
-dp = Dispatcher(bot)
-
-
-def acquire_lock():
-    try:
-        lock_fd = open(LOCK_PATH, 'w')
-        fcntl.flock(lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        return lock_fd
-    except (OSError, IOError):
-        print("Another instance of the bot is already running. Exiting.")
-        sys.exit(1)
-
-
-def release_lock(lock_fd):
-    fcntl.flock(lock_fd, fcntl.LOCK_UN)
-    lock_fd.close()
+user_message = 'Пользователь'
+admin_message = 'Админ'
+menu = 'Начать покупки'
 
 
 @dp.message_handler(commands='start')
@@ -43,39 +24,47 @@ async def cmd_start(message: types.Message):
 
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     if message.chat.id in DEFAULT_ADMINS:
-        markup.row('Пользователь', 'Админ')
+
+        markup.row(user_message, admin_message)
     else:
-        markup.row('Начать покупки')
+        markup.row(menu)
 
     await message.answer('''Привет, Hola, Привіт  👋
+
 🤖 Я ваш помощник по заказу всего вкусного и домашнего в магазине VkusDoma
 
-🛍️ Чтобы перейти в каталог продуктов и выбрать их, воспользуйтесь командой /menu.
-или нажмите внизу кнопку "Начать покупки".
+🛍️ Чтобы перейти в каталог продуктов и выбрать их возпользуйтесь командой /menu. 
+или нажмите внизу кнопку "Начать покупки". 
 
-Синяя кнопка слева всегда поможет вам, если вдруг что-то пошло не так,
+Синяя кнопка слева всегда поможем вам, если вдруг что-то пошло не так,
 например, найти корзину с покупками (на айфонах она иногда сворачивается внизу
-и выглядит как 4 кружка в нижнем меню, что не всегда очевидно для пользователя)
+и выглядит как 4 кружка в нижем меню, что не всегда очевидно для пользователя)
 
-❓ По любым вопросам пишите нам, для этого
+❓ По любым вопросам, пишите нам, для этого
 жмите /sos и спрашивайте о чем угодно.
 
-Добро пожаловать во вкусный мир!''', reply_markup=markup)
+Добро пожаловать во вкусный мир!
+
+    ''', reply_markup=markup)
 
 
 @dp.message_handler(text=admin_message)
 async def admin_mode(message: types.Message):
     status = True
-    res = requests.post('https://chtb.onrender.com/bot/admins/', data={'user_id': message.from_user.id, 'status': status})
+    res = requests.post('https://chtb.onrender.com/bot/admins/',
+                        data={'user_id': message.from_user.id, 'status': status})
     await message.answer('Включен админский режим.',
                          reply_markup=ReplyKeyboardRemove())
+
 
 @dp.message_handler(text=user_message)
 async def user_mode(message: types.Message):
     status = False
-    res = requests.post('https://chtb.onrender.com/bot/admins/', data={'user_id': message.from_user.id, 'status': status})
+    res = requests.post('https://chtb.onrender.com/bot/admins/',
+                        data={'user_id': message.from_user.id, 'status': status})
     await message.answer('Включен пользовательский режим.',
                          reply_markup=ReplyKeyboardRemove())
+
 
 @dp.message_handler(text=menu)
 async def menu_handler(message: types.Message):
@@ -85,7 +74,6 @@ async def menu_handler(message: types.Message):
 
     await process_catalog(message)
     await message.answer('Меню каталог', reply_markup=markup)
-
 
     # await bot.set_chat_menu_buttons(message.chat.id)
 
@@ -106,10 +94,6 @@ async def on_startup(dp):
     basicConfig(level=INFO)
     await set_bot_commands()
 
-if __name__ == '__main__':
-    try:
-        lock_fd = acquire_lock()
-        executor.start_polling(dp, on_startup=on_startup)
-    finally:
-        release_lock(lock_fd)
 
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
